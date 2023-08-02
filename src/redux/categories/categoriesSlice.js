77bebc8b0ab1c19b87d25cda4f044d0cc77f7a4d
mariegrace31 <mariegracebmg@gmail.com>
2023-08-02 11:00:09 +0200

    Define a slice of state for books

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -7,4 +7,14 @@ const initialState = {
 const bookSlice = createSlice({
   name: 'books',
   initialState,
+  reducers: {
+    addBook: (state,action) => [...state, action.payload],
+    removeBook: (state, action) => {
+      state.bookItems.filter((item) => item.id !== action.payload);
+    },
+  },
 });
+
+export const { addBook, removeBook } = bookSlice.actions;
+
+export default bookSlice.reducer;
