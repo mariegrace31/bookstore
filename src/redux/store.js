9aab6faafcc2250790fbec53c593d8d84c93a81e
mariegrace31 <mariegracebmg@gmail.com>
2023-08-02 11:23:29 +0200

    Update the store file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import bookReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
     books: bookReducer,
+    categories: categoriesReducer,
   },
 });
 
