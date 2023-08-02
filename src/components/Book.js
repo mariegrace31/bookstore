eded919ded05e7de68b8c1d9423ac736393b7b67
mariegrace31 <mariegracebmg@gmail.com>
2023-08-02 16:42:11 +0200

    Add the remove button component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,22 +1,30 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import RemoveBookBtn from './RemoveBookBtn';
 
 function Book({ book }) {
   return (
     <>
-      <ul>
-        <li>
-          {book.title}
-          <span>{book.author}</span>
-          <button type="submit">Delete</button>
-        </li>
-      </ul>
+      <li key={book.item_id}>
+        {book.title}
+        <span>{book.author}</span>
+        <RemoveBookBtn itemId={book.item_id} />
+        <span>{book.item_id}</span>
+      </li>
     </>
   );
 }
 
 export default Book;
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    item_id: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 Book.propTypes = {
   book: PropTypes.shape({
