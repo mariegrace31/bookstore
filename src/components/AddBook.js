1daac149ad0ea0742a48fae8c09b1abdd5b97ac0
mariegrace31 <mariegracebmg@gmail.com>
2023-08-02 16:27:18 +0200

    Dispatch actions using useDispatch

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
+
 
 function AddBook() {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState();
+  const [author, setAuthor] = useState();
+  const length = useSelector((state) => state.books.length + 1);
+
+  const handletitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleSelectChange = (e) => {
+    setAuthor(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addBook({
+      title, author, category: 'Fiction', item_id: `item${length}`,
+    }));
+  };
   return (
     <>
-      <form>
-        <input type="text" placeholder="Book Title" />
-        <select>
-          <option>Author</option>
-        </select>
-        <button type="submit">Add book</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={title} placeholder="Book Title" onChange={handletitleChange} />
+        <input type="text" value={author} placeholder="author" onChange={handleSelectChange} />
       </form>
-
     </>
   );
 }
