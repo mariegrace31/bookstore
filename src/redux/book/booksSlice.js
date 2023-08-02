5caa6505ae3f269288994fa35e4f51e70d1f8375
mariegrace31 <mariegracebmg@gmail.com>
2023-08-02 11:04:12 +0200

    Update files

---
+++ src/redux/book/booksSlice.js
@@ -0,0 +1,20 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+  bookItems: [],
+};
+
+const bookSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state,action) => [...state, action.payload],
+    removeBook: (state, action) => {
+      state.bookItems.filter((item) => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addBook, removeBook } = bookSlice.actions;
+
+export default bookSlice.reducer;
