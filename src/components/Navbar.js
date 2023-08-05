a4c00c9640344ef4d09dec89c56bd634603a5be3
mariegrace31 <mariegracebmg@gmail.com>
2023-08-05 11:17:52 +0200

    Update the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BsPersonCircle } from 'react-icons/bs';
 
 const navLinks = [
   { path: '/', text: 'Books' },
@@ -9,13 +11,21 @@ const navLinks = [
 function Navbar() {
   return (
     <>
-      {
+      <div className="navbar-container">
+        <div className="navbar">
+          <h1 className="logo">Bookstore CMS</h1>
+          <ul className="nav-items">
+            {
         navLinks.map((navLink) => (
-          <li key={navLink.text}>
-            <Link to={navLink.path}>{navLink.text}</Link>
+          <li key={navLink.text} className="nav">
+            <Link to={navLink.path} className="nav-link">{navLink.text}</Link>
           </li>
         ))
     }
+          </ul>
+        </div>
+        <BsPersonCircle className="user-icon" />
+      </div>
     </>
   );
 }
